6f60c943578fb924fc5d49b04f40ef86e8dd7d4f
Nikola Milun <nikola.milun2508@gmail.com>
2022-06-14 16:32:18 +0200

    Add project files.

---
+++ Controllers/OsobeController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ASPIntro.Data;
+using ASPIntro.Models;
+
+namespace ASPIntro.Controllers
+{
+    public class OsobeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OsobeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Osobe
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Osoba.ToListAsync());
+        }
+
+        // GET: Osobe/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var osoba = await _context.Osoba
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (osoba == null)
+            {
+                return NotFound();
+            }
+
+            return View(osoba);
+        }
+
+        // GET: Osobe/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Osobe/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BrTelefona,Ime,Id")] Osoba osoba)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(osoba);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(osoba);
+        }
+
+        // GET: Osobe/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var osoba = await _context.Osoba.FindAsync(id);
+            if (osoba == null)
+            {
+                return NotFound();
+            }
+            return View(osoba);
+        }
+
+        // POST: Osobe/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BrTelefona,Ime,Id")] Osoba osoba)
+        {
+            if (id != osoba.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(osoba);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OsobaExists(osoba.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(osoba);
+        }
+
+        // GET: Osobe/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var osoba = await _context.Osoba
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (osoba == null)
+            {
+                return NotFound();
+            }
+
+            return View(osoba);
+        }
+
+        // POST: Osobe/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var osoba = await _context.Osoba.FindAsync(id);
+            _context.Osoba.Remove(osoba);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OsobaExists(int id)
+        {
+            return _context.Osoba.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/Osoba.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPIntro.Models
+{
+    public class Osoba
+    {
+        private int id;
+        private string ime;
+        private string brTelefona;
+
+        public Osoba()
+        {
+                
+        }
+
+        public string BrTelefona
+        {
+            get { return brTelefona; }
+            set { brTelefona = value; }
+        }
+
+
+        public string Ime
+        {
+            get { return ime; }
+            set { ime = value; }
+        }
+
+        public int Id 
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+    }
+}
